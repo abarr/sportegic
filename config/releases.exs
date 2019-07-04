a2ff343e3fd45ab66c90c33e4bd03798d393fc0a
Andrew Barr <abarr@Andrews-MacBook-Pro.local>
2019-07-04 10:21:47 +1000

    config test

--- config/releases.exs
+++ config/releases.exs
@@ -1,17 +1,17 @@
 import Config
 
-secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
-mailgun_api_key = System.fetch_env!("MAILGUN_API_KEY")
-password = System.fetch_env!("PASSWORD")
-twilio_api_key = System.fetch_env!("TWILIO_API_KEY")
-twilio_secret_key = System.fetch_env!("TWILIO_API_KEY")
-google_credentials = System.fetch_env!("GOOGLE_APPLICATION_CREDENTIALS")
+# secret_key_base = System.fetch_env!("SECRET_KEY_BASE")
+# mailgun_api_key = System.fetch_env!("MAILGUN_API_KEY")
+# password = System.fetch_env!("PASSWORD")
+# twilio_api_key = System.fetch_env!("TWILIO_API_KEY")
+# twilio_secret_key = System.fetch_env!("TWILIO_API_KEY")
+# google_credentials = System.fetch_env!("GOOGLE_APPLICATION_CREDENTIALS")
 
-# secret_key_base = "OFhwto16KFhJuK6Ic8UvJ7CQ3++5EBAGegEMC1vMoxmopEALSllv/bj1c400buK/"
-# password = "kiM8s5ngsJbfBEps"
-# mailgun_api_key = "key-ef2a9b6e6cad2b2beccf0518a6068107"
-# twilio_api_key = "ACac7881bb3aa9f0bf4ccf9207cb0525cd"
-# twilio_secret_key = "4cd12ff8f4bbc19ab529f67f55d28e9d"
+secret_key_base = "OFhwto16KFhJuK6Ic8UvJ7CQ3++5EBAGegEMC1vMoxmopEALSllv/bj1c400buK/"
+password = "kiM8s5ngsJbfBEps"
+mailgun_api_key = "key-ef2a9b6e6cad2b2beccf0518a6068107"
+twilio_api_key = "ACac7881bb3aa9f0bf4ccf9207cb0525cd"
+twilio_secret_key = "4cd12ff8f4bbc19ab529f67f55d28e9d"
 # google_credentials = ""
 
 config :sportegic, SportegicWeb.Endpoint,
